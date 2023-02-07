ea32fe345821a22c049255a2eb274206a048e9e4
Felipe Valencia <pipeedev@gmail.com>
2023-02-07 16:34:24 -0500

    chore: fix

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ import { QueryOptions } from './types';
 export function queryBuilder<
   T extends Record<string, string>,
   K extends keyof T = keyof T
->(baseOptions: QueryOptions) {
-  return (options: QueryOptions) =>
+>(baseOptions?: QueryOptions) {
+  return (options?: QueryOptions) =>
     new Query<T, K>({ ...baseOptions, ...options });
 }
 

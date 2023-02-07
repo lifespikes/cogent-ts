4066d1b3b1a2d51fe39184cf9ea9946f6ea5bd2a
Felipe Valencia <pipeedev@gmail.com>
2023-02-07 16:31:29 -0500

    chore: changes on main function.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
 import Query from './query';
 import { QueryOptions } from './types';
 
-export const queryBuilder = <
+export function queryBuilder<
   T extends Record<string, string>,
   K extends keyof T = keyof T
->(
-  baseOptions?: QueryOptions
-) => {
-  return (options?: QueryOptions) =>
+>(baseOptions: QueryOptions) {
+  return (options: QueryOptions) =>
     new Query<T, K>({ ...baseOptions, ...options });
-};
+}
 
 export { Query };
